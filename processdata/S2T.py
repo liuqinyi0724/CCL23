bf997e989dea7532020fffbfda16292fce52a635
liuqinyi0724 <51394873+liuqinyi0724@users.noreply.github.com>
2023-06-09 15:47:44 +0800

    Update S2T.py

--- processdata/S2T.py
+++ processdata/S2T.py
@@ -3,7 +3,7 @@ import opencc
 converter = opencc.OpenCC('s2t.json')
 # res = converter.convert("汉字")
 res = []
-with open("BOOK.txt","r") as simplified_text:
+with open("BOOK.txt","r") as simplified_text: #PER、OFI
     readlines = simplified_text.readlines()
     for line in readlines:
         tempres = converter.convert(line)
